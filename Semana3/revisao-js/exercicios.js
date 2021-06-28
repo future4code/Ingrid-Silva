c2583409072f41d10b33698ff794f4e47b211afe
Ingrid Flack <ingrid_flack@hotmail.com>
2021-06-28 13:14:44 -0300

    Exercício 8

--- Semana3/revisao-js/exercicios.js
+++ Semana3/revisao-js/exercicios.js
@@ -55,9 +55,13 @@ function retornaNNumerosPares(n) {
 
 // EXERCÍCIO 08
 function checaTriangulo(a, b, c) {
-  // return 'Escaleno'
-  // return 'Equilátero'
-  // return 'Isósceles'
+  if (a !== b && a !== c && b !== c) {
+    return "Escaleno";
+  } else if (a === b && a === c) {
+    return "Equilátero";
+  } else {
+    return "Isósceles";
+  }
 }
 
 // EXERCÍCIO 09
