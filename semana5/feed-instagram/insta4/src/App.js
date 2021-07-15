ce4042d138289ea38439677d9bac979367d61818
Ingrid Flack <ingrid_flack@hotmail.com>
2021-07-15 14:57:44 -0300

    Exercícios com array - react

--- semana5/feed-instagram/insta4/src/App.js
+++ semana5/feed-instagram/insta4/src/App.js
@@ -13,21 +13,123 @@ const MainContainer = styled.div`
   align-items: center;
 `;
 
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin: 20px 0;
+`;
+
+const Input = styled.input`
+  padding: 10px;
+  margin: 10px 0px;
+`;
+
 class App extends React.Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        nomeUsuario: "paulinha",
+        fotoUsuario: "https://picsum.photos/50/50",
+        fotoPost: "https://picsum.photos/200/150",
+        comentarios: [],
+      },
+      {
+        id: 2,
+        nomeUsuario: "ingrid",
+        fotoUsuario: PerfilIngrid,
+        fotoPost: Post1,
+        comentarios: [],
+      },
+      {
+        id: 3,
+        nomeUsuario: "dudu",
+        fotoUsuario: PerfilDudu,
+        fotoPost: Post2,
+        comentarios: [],
+      },
+    ],
+
+    nomeUsuario: "",
+    fotoUsuario: "",
+    fotoPost: "",
+  };
+
+  adicionaPost = (event) => {
+    event.preventDefault();
+
+    const novoPost = {
+      id: this.state.posts.length + 1,
+      nomeUsuario: this.state.nomeUsuario,
+      fotoUsuario: this.state.fotoUsuario,
+      fotoPost: this.state.fotoPost,
+      comentarios: [],
+    };
+
+    const novoPosts = [...this.state.posts, novoPost];
+
+    this.setState({
+      posts: novoPosts,
+      nomeUsuario: "",
+      fotoUsuario: "",
+      fotoPost: "",
+    });
+  };
+
+  adicionaComentario = (id, comentario) => {
+    const posts = this.state.posts.map((post) => {
+      if (post.id !== id) return post;
+
+      return {
+        ...post,
+        comentarios: [...post.comentarios, comentario],
+      };
+    });
+
+    this.setState({ posts });
+  };
+
+  onChangeInputNome = (event) => {
+    this.setState({ nomeUsuario: event.target.value });
+  };
+
+  onChangeInputFoto = (event) => {
+    this.setState({ fotoUsuario: event.target.value });
+  };
+  onChangeInputPost = (event) => {
+    this.setState({ fotoPost: event.target.value });
+  };
+
   render() {
     return (
       <MainContainer>
-        <Post
-          nomeUsuario={"paulinha"}
-          fotoUsuario={"https://picsum.photos/50/50"}
-          fotoPost={"https://picsum.photos/200/150"}
-        />
-        <Post
-          nomeUsuario={"ingrid"}
-          fotoUsuario={PerfilIngrid}
-          fotoPost={Post1}
-        />
-        <Post nomeUsuario={"dudu"} fotoUsuario={PerfilDudu} fotoPost={Post2} />
+        <Form onSubmit={this.adicionaPost}>
+          <Input
+            value={this.state.nomeUsuario}
+            onChange={this.onChangeInputNome}
+            type="text"
+            placeholder="Digite o seu nome"
+          />
+          <Input
+            value={this.state.fotoUsuario}
+            onChange={this.onChangeInputFoto}
+            placeholder="Adicione a URL da sua foto"
+          />
+          <Input
+            value={this.state.fotoPost}
+            onChange={this.onChangeInputPost}
+            placeholder="Cole a URL da foto do seu Post"
+          />
+          <button>Criar Post</button>
+        </Form>
+
+        {this.state.posts.map((post) => (
+          <Post
+            key={post.id}
+            post={post}
+            adicionaComentario={this.adicionaComentario}
+          />
+        ))}
       </MainContainer>
     );
   }
