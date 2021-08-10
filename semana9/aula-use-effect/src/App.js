c32d421ac788492450ba49b3f65ffd7d401cf9bd
Ingrid Flack <ingrid_flack@hotmail.com>
2021-08-10 14:30:09 -0300

    exercicio-use-effect

---
+++ semana9/aula-use-effect/src/App.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import PokeCard from "./components/PokeCard";
+
+export default function App() {
+  const [pokeList, setPokeList] = useState([]);
+  const [pokeName, setPokeName] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
+      .then((response) => {
+        setPokeList(response.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const changePokeName = (event) => {
+    setPokeName(event.target.value);
+  };
+  return (
+    <div>
+      <select onChange={changePokeName}>
+        <option value={""}>Nenhum</option>
+        {pokeList.map((pokemon) => {
+          return (
+            <option key={pokemon.name} value={pokemon.name}>
+              {pokemon.name}
+            </option>
+          );
+        })}
+      </select>
+
+      {pokeName && <PokeCard pokemon={pokeName} />}
+    </div>
+  );
+}
