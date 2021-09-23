492a933187456f0fe6936cb7c82cd65f4ede4e96
Ingrid Flack <ingrid_flack@hotmail.com>
2021-09-23 18:30:55 -0300

    Exercicio 3 concluido

--- semana15/template-express/src/index.ts
+++ semana15/template-express/src/index.ts
@@ -12,6 +12,38 @@ app.get("/countries", (req: Request, res: Response) => {
   res.status(200).send(countries);
 });
 
+app.get("/countries/search", (req: Request, res: Response) => {
+  let result: country[] = countries;
+
+  try {
+    if (req.query.name) {
+      result = result.filter((country) =>
+        country.name.includes(req.query.name as string)
+      );
+    }
+
+    if (req.query.capital) {
+      result = result.filter((country) =>
+        country.capital.includes(req.query.capital as string)
+      );
+    }
+
+    if (req.query.continent) {
+      result = result.filter((country) =>
+        country.continent.includes(req.query.continent as string)
+      );
+    }
+
+    if (!result.length) {
+      throw new Error("Not found");
+    }
+
+    res.send(result);
+  } catch (error: any) {
+    res.status(404).send(error.message);
+  }
+});
+
 app.get("/countries/:id", (req: Request, res: Response) => {
   try {
     if (isNaN(Number(req.params.id))) {
@@ -30,6 +62,11 @@ app.get("/countries/:id", (req: Request, res: Response) => {
   } catch (error: any) {
     res.status(404).send(error.message);
   }
+});
+
+app.post("/countries/:id", (req: Request, res: Response) => {
+  try {
+  } catch (error: any) {}
 });
 
 const server = app.listen(process.env.PORT || 3003, () => {
